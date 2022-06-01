22205d27cccbde73d0d97751a572716966aec657
Thomas Gregory <ThomJGregory@gmail.com>
2022-05-31 22:16:00 -0500

    Cleaned up server file and routes file, added controller folder and file to compartmentalize and organize a bit better.

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,55 +1,14 @@
 const express = require('express');
 router = express.Router();
+const {
+	getRecipes,
+	addRecipe,
+	updateRecipe,
+	deleteRecipe
+} = require('../controllers/recipeController');
 
-router.get('/', (req, res) => {
-	res.status(200).json([
-		{
-			recipename: 'Cajun Shrimp Pasta',
-			recipedescription: 'A creamy pasta i like to eat',
-			oven: '',
-			ingredients: [
-				'Jumbo Shrimp',
-				'Cajun Seasoning',
-				'Pasta',
-				'Olive Oil',
-				'Garlic'
-			],
-			steps: [
-				'Sautee Shrimp with Garlic and Olive Oil',
-				'Add cajun seasoning and cream',
-				'Boil Noodles',
-				'Combine'
-			]
-		},
-		{
-			recipename: 'Dipping sauce',
-			recipedescription: 'Sauce I use for my fries',
-			oven: '',
-			ingredients: [
-				'Mayonaisse',
-				'Ketchup',
-				'Mustard',
-				'Onion Powder',
-				'Garlic Powder',
-				'Paprika',
-				'Worchestire',
-				'Pickle Juice'
-			],
-			steps: ['Combine ingredients']
-		}
-	]);
-});
-
-router.post('/', (req, res) => {
-	res.status(200).json({ message: 'reciperoute add recipe' });
-});
-
-router.put('/:id', (req, res) => {
-	res.status(200).json({ message: `Update recipe ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-	res.status(200).json({ message: `Delete recipe ${req.params.id}` });
-});
+// this is a succinct way of accomplishing two different methods to the same route with one line of code instead of two.
+router.route('/').get(getRecipes).post(addRecipe);
+router.route('/:id').put(updateRecipe).delete(deleteRecipe);
 
 module.exports = router;
