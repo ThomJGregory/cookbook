bea04b0a5803998cd43fea0a0d154b4dbac496bb
Thomas Gregory <ThomJGregory@gmail.com>
2022-06-16 00:42:57 -0500

    Basic react shell for the app created, looking decent.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Recipes from './pages/Recipes'
+import Header from './components/Header'
+
 function App() {
 	return (
-		<div>
-			<h1>My App</h1>
-		</div>
+		<>
+			<Router>
+				<div class="container">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Recipes />}></Route>
+						<Route path="/login" element={<Login />}></Route>
+						<Route path="/register" element={<Register />}></Route>
+					</Routes>
+				</div>
+			</Router>
+		</>
 	)
 }
 
