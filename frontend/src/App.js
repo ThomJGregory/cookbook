93daa9830dd26aa71dfa0fe5e773d14f7f0becdc
Thomas Gregory <ThomJGregory@gmail.com>
2022-06-17 21:20:52 -0500

    Added pages, navbar in React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Recipes from './pages/Recipes'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
 
 function App() {
 	return (
-		<div>
-			<h1>My App</h1>
-		</div>
+		<>
+			<Router>
+				<div className="container">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Recipes />}></Route>
+						<Route path="/register" element={<Register />}></Route>
+						<Route path="/login" element={<Login />}></Route>
+					</Routes>
+				</div>
+			</Router>
+		</>
 	)
 }
 
